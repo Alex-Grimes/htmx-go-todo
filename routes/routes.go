6b68512c545408e881e36f6690c1a44fbd920b2c
alex <alexgrimes.se@gmail.com>
2023-12-04 18:31:07 -0500

    adding stubs and gorilla mux

--- routes/routes.go
+++ routes/routes.go
@@ -1,7 +1,38 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/labstack/gommon/log"
+)
 
 func sendTodos(w http.ResponseWriter) {
 
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	// sendTodos(w)
+}
+
+func markTodo(w http.ResponseWriter, r *http.Request) {
+	// sendTodos(w)
+}
+
+func deleteTodo(w http.ResponseWriter, r *http.Request) {
+	// sendTodos(w)
+}
+
+func createTodo(w http.ResponseWriter, r *http.Request) {
+	// sendTodos(w)
+}
+
+func SetupAndRun() {
+	mux := mux.NewRouter()
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/mark", markTodo).Methods("PUT")
+	mux.HandleFunc("/delete", deleteTodo).Methods("DELETE")
+	mux.HandleFunc("/create", createTodo).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":5000", mux))
 }
